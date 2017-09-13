59bfe84e00eddb6ff33f92ed42e5882eda398461
jm <heartfeats@gmail.com>
2017-09-13 14:58:14 -0700

    add js UI logic and business logic, css, html, and empty README

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+$(function() {
+  $("#formOne").submit(function(event) {
+    var nameInput = $("input#name").val();
+    var descriptionInput = $("input#description").val();
+    var dateInput = $("input#date").val();
+    var startInput = $("input#start").val();
+    var endInput = $("input#end").val();
+
+    $(".name").text(nameInput);
+    $(".description").text(descriptionInput);
+    $(".date").text(dateInput);
+    $(".start").text(startInput);
+    $(".end").text(endInput);
+
+    $("#confirmation").show();
+
+    event.preventDefault();
+  });
+});
